62e5c3c272e078a5fd91ceb7d4d31c155b416df1
imikeliu <mike_talkben@foxmail.com>
2019-12-17 17:03:49 +0800

    创建项目

---
+++ data.js
@@ -0,0 +1,23 @@
+function jqxx_gameData()//year,repeat,credit,save,money,people,cover,hospital,school,policestation,borrowinterest,house,saveinterest,yourhospital,yourschool,yourpolicestation,GDP,tax
+{
+	this.year=1989;//年份
+	this.repeat=-1;//重复
+	this.credit=0;//欠款 元
+	this.save=0;//存款 元
+	this.money=0;//现金 元
+	this.people=0;//人口 人
+	this.cover=//面积 平方千米
+	this.hospital=//医院
+	this.school=//学校
+	this.policestation=//警察局
+	this.borrowinterest=//借钱利息
+	this.house=//住宅楼个数
+	this.business=//商务区个数
+	this.saveinterest=//存钱利息
+	this.yourhospital=//你的医院
+	this.yourschool=//你的学校
+	this.yourpolicestation=//你的警察局
+	this.GDP=//生产总值
+	this.tax=//税
+	this.happiness=//幸福指数
+}
